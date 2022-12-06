d974465a0265cade1dbbdbb44b3ffe45d9913945
Kharisma14 <kharismanisa14@gmail.com>
2022-12-07 04:12:44 +0700

    error handling + error msg

--- js/main.js
+++ js/main.js
@@ -13,8 +13,15 @@ function inputHandler(proses)
 	var textOutput = "";                                        // untuk menyimpan output
 
     // untuk vigenere
-    if(proses == "vigenere"){                                       
-        console.log(textInputV)                                       
+    if(proses == "vigenere"){     
+        if (!checkInput(textInputV) || !checkInput(keyInput) || !isAllAlphabets(keyInput)) {
+            document.getElementById("inputError").style.display = "block";
+            document.getElementById("hasil").style.display = "none";
+            clearResult();
+            return;
+        }                                  
+        console.log(textInputV);
+        console.log(keyInput);                                    
         if (encryptFlag) {                                          
             textOutput = Vigenere.encrypt(textInputV, keyInput);
         } else {
@@ -23,14 +30,28 @@ function inputHandler(proses)
     }
     // untuk rot13
     else if(proses == "rot13"){
+        if (!checkInput(textInput)) {
+            document.getElementById("inputError").style.display = "block";
+            document.getElementById("hasil").style.display = "none";
+            clearResult();
+            return;
+        }
+        console.log(textInput);                                      
         if (encryptFlag) {
-            textOutput = ROT13.encrypt(textInput, keyInput);
+            textOutput = ROT13.encrypt(textInput);
         } else {
-            textOutput = ROT13.decrypt(textInput, keyInput);
+            textOutput = ROT13.decrypt(textInput);
         }
     }
     // untuk rot13vigenere
     else if(proses == "rot13vigenere"){
+        if (!checkInput(textInputRV) || !checkInput(keyInputRV) || !isAllAlphabets(keyInputRV)) {
+            document.getElementById("inputError").style.display = "block";
+            document.getElementById("hasil").style.display = "none";
+            clearResult();
+            return;
+        }
+        console.log(textInputRV);
         if (encryptFlag) {
             const rot13 = ROT13.encrypt(textInputRV);
             textOutput = Vigenere.encrypt(rot13, keyInputRV);
@@ -46,6 +67,8 @@ function inputHandler(proses)
 }
 
 function generateAnswerPage(){
+    document.getElementById("inputError").style.display = "none";
+    document.getElementById("hasil").style.display = "block";
     document.getElementById("finalResult").innerHTML = printObject.finalResult;
 }
   
@@ -63,4 +86,13 @@ function checkAplhabet(char) {
 
 function setEncryptFlag(flag){
     encryptFlag = flag;
+}
+
+function checkInput(str){
+    if(str == ""){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
