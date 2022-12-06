db27e798da558baf320da2a4f74638f443309102
Kharisma14 <kharismanisa14@gmail.com>
2022-12-06 23:56:23 +0700

    masih gagal

--- js/main.js
+++ js/main.js
@@ -1,1 +1,154 @@
-//kosong :D
+import ROT13 from './rot13.js';
+import Vigenere from './vigenere.js';
+
+var encryptFlag = true;
+var printObject = {
+    finalResult: []
+}
+
+function inputHandler()
+{
+	var inputText = document.getElementById("text").value;
+	var inputKey = document.getElementById("key").value;
+	var textOutput;
+
+    var proses = document.getElementById("proses").value;
+
+    if(proses == "vigenere"){
+        if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+            clearResult();
+            return;
+        }
+
+        if (encryptFlag) {
+            textOutput = Vigenere.encrypt(textInput.value, keyInput.value);
+        } else {
+            textOutput = Vigenere.decrypt(textInput.value, keyInput.value);
+        }
+    }
+
+    else if(proses == "rot13"){
+        if (!keyInput) {
+            clearResult();
+            return;
+        }
+        if (encryptFlag) {
+            textOutput = ROT13.encrypt(textInput.value, keyInput.value);
+        } else {
+            textOutput = ROT13.decrypt(textInput.value, keyInput.value);
+        }
+    }
+
+    else{
+        if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+            clearResult();
+            return;
+        }
+
+        if (encryptFlag) {
+            const vigenere = Vigenere.encrypt(textInput.value, keyInput.value);
+            textOutput = ROT13.encrypt(vigenere);
+        } else {
+            const rot13 = ROT13.decrypt(textInput.value);
+            textOutput = Vigenere.decrypt(rot13, keyInput.value);
+        }
+    }
+    printObject.finalResult = textOutput;
+    generateAnswerPage();
+    return textOutput;
+}
+
+function generateAnswerPage(){
+    $("#finalResult").append(printObject.finalResult);
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const buttons = document.querySelectorAll('button');
+//     buttons.forEach(button => {
+//       button.addEventListener('click', function() {
+//         const textInput = document.querySelector('#text') || false;
+//         const keyInput = document.querySelector('#key') || false;
+//         const textValue = textInput ? textInput.value : '';
+//         const keyValue = keyInput ? keyInput.value : '';
+  
+//         if (!textInput || !textValue) {
+//           clearResult();
+//           return;
+//         }
+//         // VIGERENE 
+//         if (this.getAttribute('name') === 'vigenere') {
+//             if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+//                 clearResult();
+//                 return;
+//             }
+  
+//             if (encryptFlag) {
+//                 generateResult(Vigenere.encrypt(textInput.value, keyInput.value));
+//             } else {
+//                 generateResult(Vigenere.decrypt(textInput.value, keyInput.value));
+//             }
+//         } 
+        
+//         //ROT13
+//         else if (this.getAttribute('name') === 'rot13') {
+//             if (!keyInput) {
+//                 clearResult();
+//                 return;
+//             }
+  
+//             if (encryptFlag) {
+//                 generateResult(ROT13.encrypt(textInput.value, keyInput.value));
+//             } else {
+//                 generateResult(ROT13.decrypt(textInput.value, keyInput.value));
+//             }
+//         }
+
+//         //ROT13+VIGENERE
+//         else {
+//             if (!keyInput || !keyValue || !isAllAlphabets(keyValue)) {
+//                 clearResult();
+//                 return;
+//             }
+    
+//             if (encryptFlag) {
+//                 const vigenere = Vigenere.encrypt(textInput.value, keyInput.value);
+//                 generateResult(ROT13.encrypt(vigenere));
+//             } else {
+//                 const rot13 = ROT13.decrypt(textInput.value);
+//                 generateResult(Vigenere.decrypt(rot13, keyInput.value))
+//             }
+//         }
+//         });
+//     })
+//   });
+  
+
+
+const generateResult = (value) => {
+    const result = document.querySelector('.result');
+    result.innerHTML = `
+      <label for="input_result">Result : </label>
+      <input type="text" id="input_result" value='${value}' disabled>
+      `;
+}
+  
+function isAllAlphabets(str){
+    for (let i = 0; i < str.length; i++) {
+        if (!checkAplhabet(str[i])) return false;
+    }
+
+    return true;
+}
+  
+function checkAplhabet(char) {
+    return (/[a-zA-Z]/).test(char);
+}
+  
+function clearResult(){
+    result = document.querySelector('.result');
+    result.innerHTML = ``;
+}
+
+function setEncryptFlag(flag){
+    encryptFlag = flag;
+}
--- js/vigenere.js
+++ js/vigenere.js
@@ -1,25 +1,54 @@
-const ROT13 = {
-    job(str) {
-      const shift = 13;
+const Vigenere = {
+    encrypt(str, key) {
+      str = str.toUpperCase();
+      key = key.toUpperCase();
+      
       let result = [];
-    
+      let nonAlphabet = 0;
       for (let i = 0; i < str.length; i++) {
-          let char = str[i];
-          let temp = !this._checkAplhabet(char) ? char 
-                    : char === char.toUpperCase() ? String.fromCharCode((char.charCodeAt(0) + shift - 65) % 26 + 65)
-                    : String.fromCharCode((char.charCodeAt(0) + shift - 97) % 26 + 97)
-          
-          result.push(temp);
+        let char = str[i];
+        if (this._checkAplhabet(char)) {
+          let temp =  this._mod26((char.charCodeAt(0) - 65) + (key[(i - nonAlphabet) % key.length].charCodeAt(0) - 65)) + 65;
+          char = String.fromCharCode(temp);
+        } else {
+          nonAlphabet++;
+        }
+        result.push(char);
       }
     
       return result.join('');
     },
   
+    decrypt(cipher, key) {
+      str = str.toUpperCase();
+      key = key.toUpperCase();
+      
+      let result = [];
+      let nonAlphabet = 0;
+      for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (this._checkAplhabet(char)) {
+          let temp =  this._mod26((char.charCodeAt(0) - 65) - (key[(i - nonAlphabet) % key.length].charCodeAt(0) - 65)) + 65;
+          char = String.fromCharCode(temp);
+        } else {
+          nonAlphabet++;
+        }
+        result.push(char);
+      }
+      return result.join('');
+    },
+  
+    _mod26(num) {
+      while (num < 0) {
+        num += 26;
+      }
+    
+      return num % 26;
+    },
+    
     _checkAplhabet(char) {
       return (/[a-zA-Z]/).test(char);
     }
   }
   
-  
-  
-  export default ROT13;
+  export default Vigenere;
